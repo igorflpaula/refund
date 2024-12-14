7f334c825a06a1c28e2f62576d7633bede12ad58
igoflpaula <igor_paula12@hotmail.com>
2024-12-14 20:23:51 -0300

    create elements

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// Elementos
+const form = document.querySelector("form");
+const amount = document.getElementById("amount");
+const expense = document.getElementById("expense");
+const category = document.getElementById("category");
+
+// Elementos da lista
+const expenseList = document.querySelector("ul");
+const expenseTotal = document.querySelector("aside header h2");
+const expenseQuantity = document.querySelector("aside header p span");
