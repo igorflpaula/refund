6c945e9db4aab8e13780a9f43a5392a8d9d369e4
igoflpaula <igor_paula12@hotmail.com>
2024-12-14 20:24:57 -0300

    function expenseAdd

--- scripts.js
+++ scripts.js
@@ -38,3 +38,57 @@ form.onsubmit = (event) => {
 
   expenseAdd(newExpense);
 };
+
+// Cria a estrutura de <li>
+function expenseAdd(newExpense) {
+  try {
+    // <li>
+    const expenseItem = document.createElement("li");
+    expenseItem.classList.add("expense");
+
+    // <img>
+    const expenseIcon = document.createElement("img");
+    expenseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`);
+    expenseIcon.setAttribute("alt", newExpense.category_name);
+
+    // <div class="expense-info">
+    const expenseInfo = document.createElement("div");
+    expenseInfo.classList.add("expense-info");
+
+    // <strong>
+    const expenseName = document.createElement("strong");
+    expenseName.textContent = newExpense.expense;
+
+    // <span>
+    const expenseCategory = document.createElement("span");
+    expenseCategory.textContent = newExpense.category_name;
+
+    // Adiciona name e categoria na div das despesas (expenseInfo)
+    expenseInfo.append(expenseName, expenseCategory);
+
+    // <span><small>
+    const expenseAmount = document.createElement("span");
+    expenseAmount.classList.add("expense-amount");
+    expenseAmount.innerHTML = `<small>R$</small>${newExpense.amount
+      .toUpperCase()
+      .replace("R$", "")}`;
+
+    // <img> de delete
+    const removeIcon = document.createElement("img");
+    removeIcon.classList.add("remove-icon");
+    removeIcon.setAttribute("src", "img/remove.svg");
+
+    // Adiciona Icone, Info no Item da Lista
+    expenseItem.append(expenseIcon, expenseInfo, expenseAmount, removeIcon);
+    expenseList.append(expenseItem);
+
+    // Limpa o form
+    formClear();
+
+    // Atualiza o total de items
+    updateTotals();
+  } catch (error) {
+    alert("Não foi possível atualizar a lista de despesas.");
+    console.log(error);
+  }
+}
