3b5c199d8dda80c087a8f7056bc65347654652eb
igoflpaula <igor_paula12@hotmail.com>
2024-12-14 20:24:38 -0300

    function on submit

--- scripts.js
+++ scripts.js
@@ -23,3 +23,18 @@ function formatCurrencyBRL(value) {
     currency: "BRL",
   });
 }
+
+form.onsubmit = (event) => {
+  event.preventDefault();
+
+  const newExpense = {
+    id: new Date().getTime(),
+    expense: expense.value,
+    category_id: category.value,
+    category_name: category.options[category.selectedIndex].text,
+    amount: amount.value,
+    created_at: new Date(),
+  };
+
+  expenseAdd(newExpense);
+};
